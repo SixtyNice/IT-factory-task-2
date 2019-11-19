1205932043c2e38492ed1559e8b00b0ff832a1d6
Dima Skibin <sixtynice77@gmail.com>
2019-11-19 13:00:30 +0300

    Create some new functions to create elements and add new node to document

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ function createTagElement(className, tag, text) {
     return element;
 }
 
+function createOptions(arrayFilters) {
+    let filterFormat = [];
+    for (let i = 0; i < arrayFilters.length; i++) {
+        filterFormat.push(createTagElement(arrayFilters[i].toLowerCase(), "option", `${arrayFilters[i]}`));
+    }
+    return filterFormat;
+}
+
+
+function createLink() {
+
+}
+
+function createImg() {
+
+}
+
 
 let header = createTagElement('header', 'header');
 
@@ -22,23 +39,27 @@ header.appendChild(createTagElement("header-inner"))
     .appendChild(createTagElement("header-nav", "nav"))
     .append(
         createTagElement("header__link header__link-search", "a", "Search"),
-        createTagElement("header__link header__link-my-movies", "a", "My Movies"))
+        createTagElement("header__link header__link-my-movies", "a", "My Movies"));
 
 let headerUser = createTagElement('header__user');
 headerUser.append(createTagElement("header__username", "span", "Dima Skibin"),
     createTagElement("header__user-avatar", "img"));
 
 let container = createTagElement("container");
-container.appendChild(createTagElement("main-container", "main"))
+// createTagElement("main-container", "main")
+container.appendChild(
+    document.createElement("div")
     .appendChild(createTagElement("search-container", "section"))
     .appendChild(createTagElement("filters"))
-    .append(createTagElement("filter filters__format", "select"))
+    .appendChild(createTagElement("filter filters__format", "select"))
+    .append(...createOptions(["Movie", "Series", "Сartoon"])), 
 
-let filtersFormatArray = ["Movie", "Series", "Сartoon"];
-let filterFormat;
-for (let i = 0; i < filtersFormatArray.length; i++) {
-    filterFormat = 
-}
+    createTagElement("filter filters__genre", "select")
+    .append(...createOptions(["Horror", "Drama", "Comedy", "Thriller"])));
 
-document.body.append(header, header__user, container);
+
+
+
+
+document.body.append(header, headerUser, container);
 
