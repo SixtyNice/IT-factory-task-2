e57026febd624abd8c877e18cb4939b9a80f11ed
Dima Skibin <sixtynice77@gmail.com>
2019-11-24 15:04:50 +0300

    Made main html file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function createTagElement(className, tag, text) {
+function createTagElement(className, tag, text, attribute, value) {
     let element;
     if (tag) {
         element = document.createElement(tag);
@@ -10,15 +10,18 @@ function createTagElement(className, tag, text) {
         element.classList = className;
     }
     if (text) {
-        element.innerText = text;
+        element.innerHTML = text;
+    }
+    if (attribute) {
+        element.setAttribute(attribute, value);
     }
     return element;
 }
 
-function createOptions(arrayFilters) {
+function createOptions(arrayOptions) {
     let filterFormat = [];
-    for (let i = 0; i < arrayFilters.length; i++) {
-        filterFormat.push(createTagElement(arrayFilters[i].toLowerCase(), "option", `${arrayFilters[i]}`));
+    for (let i = 0; i < arrayOptions.length; i++) {
+        filterFormat.push(createTagElement(arrayOptions[i].toLowerCase(), "option", `${arrayOptions[i]}`));
     }
     return filterFormat;
 }
@@ -46,20 +49,71 @@ headerUser.append(createTagElement("header__username", "span", "Dima Skibin"),
     createTagElement("header__user-avatar", "img"));
 
 let container = createTagElement("container");
-// createTagElement("main-container", "main")
-container.appendChild(
-    document.createElement("div")
-    .appendChild(createTagElement("search-container", "section"))
-    .appendChild(createTagElement("filters"))
+let mainContainer = createTagElement("main-container", "main");
+let searchContainer = createTagElement("search-container", "section");
+let filtersContainer = createTagElement("filters");
+
+container.appendChild(mainContainer)
+    .appendChild(searchContainer)
+    .appendChild(filtersContainer)
     .appendChild(createTagElement("filter filters__format", "select"))
-    .append(...createOptions(["Movie", "Series", "Сartoon"])), 
+    .append(...createOptions(["Movie", "Series", "Сartoon"]));
 
-    createTagElement("filter filters__genre", "select")
-    .append(...createOptions(["Horror", "Drama", "Comedy", "Thriller"])));
+filtersContainer.appendChild(createTagElement("filter filters__genre", "select"))
+    .append(...createOptions(["Horror", "Drama", "Comedy", "Thriller"]));
+
+filtersContainer.appendChild(createTagElement("filter filters__year", "select"))
+    .append(...createOptions(createArrayOfYears()));
+
+function createArrayOfYears() {
+    let array = [];
+    for (let i = 1905; i < 2019; i++) {
+        array.push(i.toString());
+    }
+    return array.reverse();
+}
 
 
+searchContainer
+    .appendChild(createTagElement("search"))
+    .appendChild(createTagElement("search__field", "input", "", "placeholder", "Search"))
+    .after(createTagElement("search__button", "button", "Search"))
 
+let table = createTagElement("table-movies", "table");
+mainContainer
+    .appendChild(createTagElement("list-movies", "section"))
+    .appendChild(table)
+    .appendChild(createTagElement("field", "tr"))
+    .append(
+        createTagElement("picture-movie", "th"),
+        createTagElement("title-movie", "th", "Title"),
+        createTagElement("format-movie", "th", "Format"),
+        createTagElement("genre-movie", "th", "Genre"),
+        createTagElement("year-movie", "th", "Year"),
+        createTagElement("rating-movie", "th", "Rating")
+    );
+for (let i = 0; i < 4; i++) {
+    table.appendChild(createTagElement("field", "tr"))
+        .append(
+            createTagElement("", "td"),
+            createTagElement("", "td", "Some"),
+            createTagElement("", "td", "Movies"),
+            createTagElement("", "td", "Drama"),
+            createTagElement("", "td", "1987"),
+            createTagElement("", "td", "3")
+        );
+}
 
+let navBtns=[];
+for(let i = 0;i<6;i++) {
+    navBtns.push(createTagElement("nav-btn page-num","div",i.toString()))
+}
+
+mainContainer.appendChild(createTagElement("nav-wrapper"))
+    .appendChild(createTagElement("nav-pages"))
+    .append(createTagElement("nav-btn prev-page","div","<<"),
+    ...navBtns,
+    createTagElement("nav-btn next-page","div",">>"));
 
 document.body.append(header, headerUser, container);
 
